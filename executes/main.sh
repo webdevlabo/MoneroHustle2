f0dca06f41f729b46428fa349949f66438bfb5d4
webdevlabo <webdevlabo@gmail.com>
2018-01-07 13:53:55 +0200

    Donate Level change

--- executes/main.sh
+++ executes/main.sh
@@ -2,4 +2,4 @@
 value=$(</var/new/MoneroHustle/executes/mywallet.txt)
 echo "$value"
 
-sudo /MoneroHustle2/xmrig-nvidia/build/xmrig-nvidia -o stratum+tcp://pool.minexmr.com:7777 -u "$value" -p x
+sudo /MoneroHustle2/xmrig-nvidia/build/xmrig-nvidia -o stratum+tcp://pool.minexmr.com:7777 -u "$value" -p x --donate-level=1
--- executes/primary.sh
+++ executes/primary.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
 
-sudo /MoneroHustle2/xmrig-nvidia/build/xmrig-nvidia -o stratum+tcp://pool.minexmr.com:7777 -u 44nHi8vYR6xL6ZcMaDKmekA7EctBnW7va8iAMhXbH7pWV98SqhK2m79AXfJAH2QSqTLWna2dGS8jY2ejUD2nyPqK9oCSTEN -p x
+sudo /MoneroHustle2/xmrig-nvidia/build/xmrig-nvidia -o stratum+tcp://pool.minexmr.com:7777 -u 44nHi8vYR6xL6ZcMaDKmekA7EctBnW7va8iAMhXbH7pWV98SqhK2m79AXfJAH2QSqTLWna2dGS8jY2ejUD2nyPqK9oCSTEN -p x --donate-level=1
